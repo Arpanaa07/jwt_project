d83f12277a0f7aecb8445d7eb95cda94b18e8930
arpana9 <arpanabhattarai007@gmail.com>
2025-07-25 17:12:45 +0545

    Initial commit

---
+++ jwt_project/urls.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.shortcuts import redirect
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+# Redirect root URL to Swagger UI
+def root_redirect(request):
+    return redirect('/api/docs/swagger/')
+
+urlpatterns = [
+    path('', root_redirect),  # Redirect '/' to Swagger docs
+    path('admin/', admin.site.urls),
+
+    # JWT Authentication endpoints
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    # API schema and documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Include your app URLs
+    path('api/', include('accounts.urls')),
+]
