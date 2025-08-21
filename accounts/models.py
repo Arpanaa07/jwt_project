780c992955eece85d1a11b8dc6c331756d44291a
arpana9 <arpanabhattarai007@gmail.com>
2025-08-21 15:14:06 +0545

    Initial commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=100)
+    contact_person_name = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=50)
+    sku = models.CharField(max_length=50, unique=True)
+    photo = models.ImageField(upload_to='products/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE, related_name="products")
+
+    def __str__(self):
+        return self.name
+
